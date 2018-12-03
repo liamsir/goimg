ff12ee32a8504ca61dc3d368877081ff964f975f
ismajl <helloismajl@gmail.com>
2018-12-04 00:57:30 +0100

    cache

--- main.go
+++ main.go
@@ -2,20 +2,25 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"database/sql"
 	"fmt"
 	"hash/fnv"
 	"image/jpeg"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"os/exec"
 	"strconv"
 	"strings"
 
+	"cloud.google.com/go/storage"
 	"github.com/lib/pq"
 	"github.com/nfnt/resize"
+	"google.golang.org/api/option"
 )
 
 func hash(s string) uint32 {
@@ -56,19 +61,17 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	resourceModifiers := paramResource + paramModifiers
 	resModHash := fmt.Sprint(hash(resourceModifiers))
 	resHash := fmt.Sprint(hash(paramResource))
-
 	// 1. Check if resource exists in cache for given parameters
 	connStr := "postgres://jxbnzxtecqvcsv:9f603a3b7a60b5583f668fa2cf0ab0badd2c8f9dbacc73564cb1e9ee45241312@ec2-54-246-85-234.eu-west-1.compute.amazonaws.com:5432/dag2mo4a48vlb3"
 	db, err := sql.Open("postgres", connStr)
 	rows, err := db.Query(`select f.hash, uf.user_id, f.type, f.allowed_operations from file f
 join user_file uf on uf.file_id = f.id
-where f.hash = $1 or f.hash = $2	and uf.user_id = (select id from "user" where username = $3)`,
+where (f.hash = $1 and f.type = 0) or f.hash = $2 and uf.user_id = (select id from "user" where username = $3)`,
 		resHash, resModHash, user)
 	if err, ok := err.(*pq.Error); ok {
 		fmt.Println("pq error:", err.Code.Name())
 	}
-	fmt.Println("resHash", resHash)
-	fmt.Println("resModHash", resModHash)
+
 	var orHash string
 	var mdfHash string
 	var userId int32
@@ -94,7 +97,6 @@ where f.hash = $1 or f.hash = $2	and uf.user_id = (select id from "user" where u
 			mdfHash = hash
 		}
 	}
-
 	if mdfHash != "" {
 		// get file from storage using hash and return as image
 		providerUrl := fmt.Sprintf(
@@ -114,14 +116,13 @@ where f.hash = $1 or f.hash = $2	and uf.user_id = (select id from "user" where u
 		w.Header().Set("Content-Length", strconv.Itoa(len(buf)))
 		w.Header().Set("Content-Type", "image/jpeg")
 		w.Write(buf)
+		return
 	}
 
 	// original resource
 	// 2. Check if user has permissions to perform transformations
 	// add allowed operations in file table on hash form
 	//...
-	fmt.Print("orHash ", orHash)
-	fmt.Println("allowedOperations", allowedOperations)
 
 	if userHasPermissionsToPerformOperations(allowedOperations, paramModifiers) {
 		// 3. Check if resource exists for user
@@ -167,12 +168,71 @@ where f.hash = $1 or f.hash = $2	and uf.user_id = (select id from "user" where u
 				log.Fatal(err)
 			}
 
-			// 5. Perform transformations, and save transformed image to blob and db
+			// 5. Perform transformations,
+			// and save transformed image to blob and db
 			newImage := resize.Resize(uint(width), uint(height), img, resize.Lanczos3)
 
 			bufOut := new(bytes.Buffer)
 			err = jpeg.Encode(bufOut, newImage, nil)
 			sendBuf := bufOut.Bytes()
+
+			ctx := context.Background()
+			client, err := storage.NewClient(ctx, option.WithCredentialsFile("MyProject-89e0f34eb7a6.json"))
+
+			if err != nil {
+				log.Fatalf("Failed to create client: %v", err)
+			}
+			// Sets the name for the new bucket.
+			bucketName := "imgmdf"
+			// Creates a Bucket instance.
+			bucket := client.Bucket(bucketName)
+			obj := bucket.Object("1/" + resModHash)
+			wc := obj.NewWriter(ctx)
+
+			if _, err = io.Copy(wc, bufOut); err != nil {
+				panic(err)
+			}
+			if err := wc.Close(); err != nil {
+				panic(err)
+			}
+
+			acl := obj.ACL()
+			if err := acl.Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+				panic(err)
+			}
+			//save metadata in db
+			sqlStatement := `
+			INSERT INTO public.file
+				(created_at, modified_at, last_opened, guid, hash, "index", parent_id, visible, "name", description, "type", allowed_operations, performed_operations)
+				VALUES(now(), now(), now(), $1, $2, 0, null, true, $3, '', 1, '', $4) RETURNING id;`
+			fileGuid, err := exec.Command("uuidgen").Output()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Printf("%s", fileGuid)
+			var newFileId int
+			errIn := db.QueryRow(sqlStatement, fileGuid, resModHash, paramResource, resourceModifiers).Scan(&newFileId)
+			if errIn != nil {
+				log.Fatal(errIn)
+			}
+
+			insertUserFile := `
+			INSERT INTO public.user_file
+			(user_id,
+			 	file_id,
+				"type",
+			 	"role",
+				created_at,
+			  modified_at, visible) VALUES($1, $2, 1, 0, now(), now(), true);`
+			fmt.Println("New record ID is:", newFileId)
+
+			//userId when resource is url
+			_, errInsert := db.Exec(insertUserFile, 1, newFileId)
+			if errInsert != nil {
+				panic(err)
+			}
+
 			// 6. Return edited image
 			w.Header().Set("Content-Length", strconv.Itoa(len(sendBuf)))
 			w.Header().Set("Content-Type", "image/jpeg")
