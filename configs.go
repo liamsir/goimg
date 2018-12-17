16dfab1df2f1ff28655fd3d95ba81f10ad7d42af
ismajl <helloismajl@gmail.com>
2018-12-17 23:42:02 +0100

    connection db

--- configs.go
+++ configs.go
@@ -1,5 +1,6 @@
 package main
 
-// var connectionString string = "postgres://jxbnzxtecqvcsv:9f603a3b7a60b5583f668fa2cf0ab0badd2c8f9dbacc73564cb1e9ee45241312@ec2-54-246-85-234.eu-west-1.compute.amazonaws.com:5432/dag2mo4a48vlb3"
-var connectionString string = "postgresql://rootgoimgserver:9f603a3b7a60b5583f66@goimgserver.cglo5epcd2hd.us-east-2.rds.amazonaws.com:5432/goimgserver"
+var connectionString string = "postgres://jxbnzxtecqvcsv:9f603a3b7a60b5583f668fa2cf0ab0badd2c8f9dbacc73564cb1e9ee45241312@ec2-54-246-85-234.eu-west-1.compute.amazonaws.com:5432/dag2mo4a48vlb3"
+
+// var connectionString string = "postgresql://rootgoimgserver:9f603a3b7a60b5583f66@goimgserver.cglo5epcd2hd.us-east-2.rds.amazonaws.com:5432/goimgserver"
 var storageBucketUrl string = "https://storage.googleapis.com/imgmdf"
