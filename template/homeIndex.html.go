0222c474f5939c027a87b05ea1c5c793fe452300
ismajl <helloismajl@gmail.com>
2019-01-07 12:55:36 +0100

    ndryshime

--- template/homeIndex.html.go
+++ template/homeIndex.html.go
@@ -11,7 +11,7 @@ func HomeIndex(buffer *bytes.Buffer) {
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hello Bulma!</title>
+    <title>Hello World!</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
     <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
